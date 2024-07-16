8f78e642d43c910114f43379e0a74875fa9458b3
cauvray <claire.auvray@eviden.com>
2024-07-16 17:24:33 +0200

    fix: color on hover

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -10,14 +10,14 @@
 </script>
 
 <div class="flex flex-col gap-1">
-	{#each ['azertyuiop', 'qsdfghjklm', 'wxcvbn'] as row}
+	{#each ['AZERTYUIOP', 'QSDFGHJKLM', 'WXCVBN'] as row}
 		<div class="flex gap-1 justify-center">
 			{#each row as letter}
-				{@const missing = tusmo.keyboard.get(letter.toLocaleUpperCase()) === PositionEnum.MISSING}
-				{@const exact = tusmo.keyboard.get(letter.toLocaleUpperCase()) === PositionEnum.EXACT}
-				{@const close = tusmo.keyboard.get(letter.toLocaleUpperCase()) === PositionEnum.CLOSE}
+				{@const missing = tusmo.keyboard.get(letter) === PositionEnum.MISSING}
+				{@const exact = tusmo.keyboard.get(letter) === PositionEnum.EXACT}
+				{@const close = tusmo.keyboard.get(letter) === PositionEnum.CLOSE}
 				<button
-					class={`p-2 w-8 hover:bg-slate-200 duration-200 ${exact ? 'bg-primary text-white' : 'bg-white'} ${close && 'border-2 border-primary'} ${missing && 'bg-slate-200 text-slate-400'}`}
+					class={`p-2 w-8 ${exact ? 'hover:bg-primary-dark' : 'hover:bg-slate-200'} duration-200 ${exact ? 'bg-primary text-white' : 'bg-white'} ${close && 'border-2 border-primary'} ${missing && 'bg-slate-200 text-slate-400'}`}
 					aria-label={letter}
 					on:click|preventDefault={() => dispatch('update', letter)}
 				>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,31 +12,37 @@ export default {
 		createThemes({
 			blue: {
 				primary: colors.blue[900],
+				'primary-dark': colors.blue[950],
 				secondary: colors.orange[500],
 				'secondary-dark': colors.orange[600]
 			},
 			purple: {
 				primary: colors.purple[800],
+				'primary-dark': colors.purple[900],
 				secondary: colors.orange[500],
 				'secondary-dark': colors.orange[600]
 			},
 			teal: {
 				primary: colors.teal[600],
+				'primary-dark': colors.teal[700],
 				secondary: colors.orange[500],
 				'secondary-dark': colors.orange[600]
 			},
 			amber: {
 				primary: colors.amber[600],
+				'primary-dark': colors.amber[700],
 				secondary: colors.blue[600],
 				'secondary-dark': colors.blue[700]
 			},
 			yellow: {
 				primary: colors.yellow[500],
+				'primary-dark': colors.yellow[600],
 				secondary: colors.purple[600],
 				'secondary-dark': colors.purple[700]
 			},
 			pink: {
 				primary: colors.pink[700],
+				'primary-dark': colors.pink[800],
 				secondary: colors.lime[500],
 				'secondary-dark': colors.lime[600]
 			}
